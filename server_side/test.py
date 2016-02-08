c6491976a4dd5a09bb70d0a0d49396fb66230412
Andrey <andrey@Administrators-Mac-mini.local>
2016-02-08 17:20:50 +0200

    add request/response samples

--- server_side/test.py
+++ server_side/test.py
@@ -1,9 +1,32 @@
 from flask import Flask
 from flask import render_template
+import json
+from flask import request
+from wavelets.wavelets import __all__
 app = Flask(__name__)
 
 @app.route('/')
 def hello_page(name = None):
     return render_template('landing.html', name=name)
+
+
+@app.route('/wavelets', methods=['POST'])
+def show_wavelets():
+    response = json.dumps(__all__)
+    print(response)
+    return response
+
+@app.route('/sendRequest', methods=['POST'])
+def requestResponse():
+    print(request.form)
+    response = json.dumps(request.form)
+    print(response)
+    return response
+
+@app.route('/getpersonbyid', methods = ['POST'])
+def getPersonById():
+    personId = int(request.form['personId'])
+    return str(personId)
+
 if __name__ == '__main__':
     app.run(debug=True)
