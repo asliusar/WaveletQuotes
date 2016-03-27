5a9208dec757d97f091adaabb54b358a9bbacc0b
JobyKK <jobyand121@gmail.com>
2016-03-27 15:44:23 +0300

    FIX DAT BUG

--- waveletMaker.py
+++ waveletMaker.py
@@ -16,28 +16,32 @@ common_folder = 'static/results/'
 input_plot_name = 'input_plot'
 hurst_plot_name = 'hurst_plot'
 
+
 def bytespdate2num(fmt, encoding='utf-8'):
     strconverter = mdates.strpdate2num(fmt)
+
     def bytesconverter(b):
         s = b.decode(encoding)
         return strconverter(s)
+
     return bytesconverter
+
 
 def loadStock(stock, wrange):
     '''
         Use this to dynamically pull a stock:
     '''
-    stockFile =[]
+    stockFile = []
     try:
-        print('Currently Pulling',stock)
-        urlToVisit = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range='+wrange+'/csv'
+        print('Currently Pulling', stock)
+        urlToVisit = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + stock + '/chartdata;type=quote;range=' + wrange + '/csv'
         print('URL', urlToVisit)
         try:
             sourceCode = urllib.request.urlopen(urlToVisit).read().decode()
             splitSource = sourceCode.split('\n')
             for eachLine in splitSource:
                 splitLine = eachLine.split(',')
-                if len(splitLine)==6:
+                if len(splitLine) == 6:
                     if 'values' not in eachLine:
                         stockFile.append(eachLine)
         except Exception as e:
@@ -46,20 +50,21 @@ def loadStock(stock, wrange):
         print(str(e), 'failed to pull pricing data')
     return stockFile
 
+
 def mainLoop(stock, wrange):
     try:
         date, closep, highp, lowp, openp, volume = prepareData(stock, wrange)
         x = closep
         folder_name = stock + '_' + wrange
-        plot_name = common_folder + folder_name + '/'+input_plot_name+'.png'
-        hurst_name = common_folder + folder_name + '/'+hurst_plot_name+'.png'
+        plot_name = common_folder + folder_name + '/' + input_plot_name + '.png'
+        hurst_name = common_folder + folder_name + '/' + hurst_plot_name + '.png'
         if not os.path.exists(common_folder + folder_name):
             os.makedirs(common_folder + folder_name)
         print("Main thread test")
-        showPlot(date,x,plot_name)
+        showPlot(date, x, plot_name)
         hurst_res = hurst(x)
-        print("Hurst size/input size: ",len(hurst_res),len(date[-len(hurst_res):]))
-        showPlot(date[-len(hurst_res):],hurst_res,hurst_name)
+        print("Hurst size/input size: ", len(hurst_res), len(date[-len(hurst_res):]))
+        showPlot(date[-len(hurst_res):], hurst_res, hurst_name)
         for wavelet in all_wavelets:
             wa = WaveletAnalysis(data=x, wavelet=wavelet())
             # wavelet power spectrum
@@ -81,40 +86,43 @@ def mainLoop(stock, wrange):
             # t = wa.time
             # reconstruction of the original data
             # rx = wa.reconstruction()
-            showResult(date[-len(hurst_res):], scales, power, 5, '', common_folder + folder_name + '/' + wavelet.__name__ + '_hurst.png')
+            showResult(date[-len(hurst_res):], scales, power, 5, '',
+                       common_folder + folder_name + '/' + wavelet.__name__ + '_hurst.png')
     except Exception as e:
         import traceback
         traceback.print_exc()
 
-def calculateWavelet(stock, wrange, wavelet_name,moving_avg_width):
+
+def calculateWavelet(stock, wrange, wavelet_name, moving_avg_width):
     # try:
-        date, closep, highp, lowp, openp, volume = prepareData(stock, wrange)
-        x = closep
-        for i in range(moving_avg_width-1,len(x)):
-            for j in range(i-moving_avg_width+1, i):
-                x[i] += x[j]
-            x[i] /= moving_avg_width
+    date, closep, highp, lowp, openp, volume = prepareData(stock, wrange)
+    x = closep
+    for i in range(moving_avg_width - 1, len(x)):
+        for j in range(i - moving_avg_width + 1, i):
+            x[i] += x[j]
+        x[i] /= moving_avg_width
 
-        time_scale = int(wrange[:-1])
-        folder_name = stock + '_' + wrange
+    time_scale = int(wrange[:-1])
+    folder_name = stock + '_' + wrange
 
-        plot_name = common_folder + folder_name + '/'+input_plot_name+'.png'
-        hurst_name = common_folder + folder_name + '/'+hurst_plot_name+'.png'
-        wavelet = next((x for x in all_wavelets if x.__name__ == wavelet_name), None)
+    plot_name = common_folder + folder_name + '/' + input_plot_name + '.png'
+    hurst_name = common_folder + folder_name + '/' + hurst_plot_name + '.png'
+    wavelet = next((x for x in all_wavelets if x.__name__ == wavelet_name), None)
 
-
-        if not os.path.exists(common_folder + folder_name):
-            os.makedirs(common_folder + folder_name)
-        showPlot(date,x,plot_name)
-        showPlot(date,hurst(x),hurst_plot_name)
-        wa = WaveletAnalysis(data=x, wavelet=wavelet())
-        # wavelet power spectrum
-        power = wa.wavelet_power
-        # scales
-        scales = wa.scales
-        showResult(date, scales, power, math.ceil(time_scale/4.), '', common_folder + folder_name + '/' + wavelet.__name__ + '.png')
+    if not os.path.exists(common_folder + folder_name):
+        os.makedirs(common_folder + folder_name)
+    showPlot(date, x, plot_name)
+    showPlot(date, hurst(x), hurst_plot_name)
+    wa = WaveletAnalysis(data=x, wavelet=wavelet())
+    # wavelet power spectrum
+    power = wa.wavelet_power
+    # scales
+    scales = wa.scales
+    showResult(date, scales, power, math.ceil(time_scale / 4.), '',
+               common_folder + folder_name + '/' + wavelet.__name__ + '.png')
     # except Exception as e:
     #     print('mainLoop', str(e))
+
 
 def prepareData(stock, wrange):
     stockFile = loadStock(stock, wrange)
@@ -140,16 +148,19 @@ def showResult(date, scales, power, time_scale, window, file_name):
     # fig.show()
     # fig.waitforbuttonpress()
 
+
 def showPlot(date, data, file_name):
-    plt.plot(date, data)
-    plt.savefig(file_name)
+    fig, ax = plt.subplots()
+    ax.plot(date, data)
+    fig.savefig(file_name)
+    plt.close(fig)
+
 
 def hurst(ts):
     # Create the range of lag values
     hurst_ts = np.zeros((0,), dtype=np.int)
-    for tail in range(10,len(ts)):
-
-        lags = range(1, min(10,tail//2))
+    for tail in range(10, len(ts)):
+        lags = range(1, min(10, tail // 2))
         # print(lags)
         # Calculate the array of the variances of the lagged differences
         cur_ts = ts[:tail]
@@ -161,7 +172,8 @@ def hurst(ts):
 
         # Return the Hurst exponent from the polyfit output
         # print(poly[0]*2.0)
-        hurst_ts = np.append(hurst_ts,poly[0]*2.0)
+        hurst_ts = np.append(hurst_ts, poly[0] * 2.0)
     return hurst_ts
+
 
 mainLoop('usdeur=x', '20y')
