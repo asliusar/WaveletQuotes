51f91012cb54a402a453c3177772987ee919dfc9
JobyKK <jobyand121@gmail.com>
2016-04-20 08:21:23 +0300

    add date

--- core/tools.py
+++ core/tools.py
@@ -38,18 +38,22 @@ def showResult(date, scales, power, time_scale, window, file_name):
     # fig.waitforbuttonpress()
 
 
-def split_timeline(line):
+def split_timeline(line, date):
     result = []
+    result_date = []
     t = -np.sign(line[0])
     last_index = 0
     for i, x in enumerate(line):
         if x*t >= 0:
-            t = -t
-            result.append(line[last_index:i])
+            result.append(line[last_index:i+1])
+            result_date.append(date[last_index:i+1])
             last_index = i
+        if x*t > 0:
+            t = -t
     result.append(line[last_index:len(line)])
+    result_date.append(date[last_index:len(line)])
 
-    return result
+    return result, result_date
 
 def showPlot(date, data, file_name):
     fig, ax = plt.subplots()
@@ -57,7 +61,13 @@ def showPlot(date, data, file_name):
     # fig.plot([1, 2, 3], [1, 1, 1])
     fig.savefig(file_name)
     plt.close(fig)
-
-# x = [1,4, 0, 3, -1,0,-5,x -2, 4, 5]
 #
-# print(split_timeline(x))
+# x = [1,4, 0, 3, -1, 0, -5, -2, 4, 5]
+# print(split_timeline(x, np.arange(len(x))))
+# splt_x, splt_date = split_timeline(x, np.arange(len(x)))
+# fig, ax = plt.subplots()
+# ax.plot(np.arange(len(x)), x)
+# for i, tx in enumerate(splt_x):
+#     ax.plot(splt_date[i], tx)
+# fig.savefig('test.png')
+# plt.close(fig)
