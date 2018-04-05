df46b1d925d05b94d32874f5d8d9806e86f87e88
Andrii Sliusar <ASliusar@luxoft.com>
2018-04-05 14:10:20 +0300

    fixes

---
+++ frontend/components/analisys/controls.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import autobind from "autobind-decorator";
+import AutoComplete from "material-ui/AutoComplete";
+import styles from "./css/styles.css";
+import {changeCurrencyPair} from "../../actions/filter";
+
+const propTypes = {
+    dispatch: React.PropTypes.func.isRequired,
+    filter: React.PropTypes.object
+};
+
+
+export class Controls extends React.Component {
+
+    @autobind
+    handleCurrencyPairChangeEvent(event, pair) {
+        console.log(event, pair);
+        const {dispatch} = this.props;
+        dispatch(changeCurrencyPair(event));
+    };
+
+
+    render() {
+        const classes = [styles.container, styles.input_field].join(" ");
+
+        return (
+            <span className={classes}>
+                <AutoComplete
+                    hintText="Currency pair"
+                    dataSource={this.props.filter.commonCurrencies}
+                    onUpdateInput={this.handleCurrencyPairChangeEvent}
+                />
+            </span>
+        );
+    }
+}
+
+Controls.propTypes = propTypes;
+export default Controls;
